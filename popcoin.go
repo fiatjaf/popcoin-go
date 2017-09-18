4748ae361bec2a0a76aa78b6876a2ab0172660b0
fiatjaf <fiatjaf@gmail.com>
2017-09-18 12:28:11 +0000

    support spend descriptions and list.

--- popcoin.go
+++ popcoin.go
@@ -72,13 +72,14 @@ type IdentifyResponse struct {
 	Message string `json:"message"`
 }
 
-func (c Client) Spend(user string, amount float64) (SpendResponse, error) {
+func (c Client) Spend(user string, amount float64, desc string) (SpendResponse, error) {
 	r := SpendResponse{}
 	werr := Error{}
 	_, err := c.Post(base+"/spend", struct {
-		User   string  `json:"user"`
-		Amount float64 `json:"amount"`
-	}{user, amount}, &r, &werr)
+		User        string  `json:"user"`
+		Amount      float64 `json:"amount"`
+		Description string  `json:"description"`
+	}{user, amount, desc}, &r, &werr)
 	if err != nil {
 		return r, err
 	}
@@ -95,6 +96,34 @@ type SpendResponse struct {
 		Available string `json:"available"`
 		Current   string `json:"current"`
 	} `json:"dev"`
+}
+
+func (c Client) ListSpends(user, gte, lte string) (ListSpendsResponse, error) {
+	r := ListSpendsResponse{}
+	werr := Error{}
+	params := napping.Params{
+		"user": user,
+		"gte":  gte,
+		"lte":  lte,
+	}.AsUrlValues()
+	_, err := c.Get(base+"/spend", &params, &r, &werr)
+	if err != nil {
+		return r, err
+	}
+	if werr.Status == "error" {
+		return r, werr
+	}
+	return r, nil
+}
+
+type ListSpendsResponse struct {
+	Status string `json:"status"`
+	Spends []struct {
+		Id          string  `json:"_id"`
+		Amount      float64 `json:"amount"`
+		Description string  `json:"description"`
+		SpentAt     string  `json:"spent_at"`
+	} `json:"spends"`
 }
 
 type Error struct {
